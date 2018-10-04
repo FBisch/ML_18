5adee6156ba9feeeb2a9b1562dbc480fde355888
timothee-florian <timothee-florian.bronner@epfl.ch>
2018-10-04 12:43:00 +0200

    add functions.py containing the asked for functions

---
+++ functions.py
@@ -0,0 +1,52 @@
+def compute_loss(y, tx, w):
+    """Calculate the loss (MSE)."""
+    N= tx.shape[0]
+    e = y - np.matmul(tx, w)
+    return np.sum(e * e)/ (2 * N)
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    N= tx.shape[0]
+    e = y - np.matmul(tx, w)
+    return  -1 * np.dot(tx.T, e) /N 
+
+def leasts_squares(y, tx):
+    X2 = np.matmul(np.transpose(tx), tx)
+    X2_inv = np.linalg.inv(X2)
+    W = np.matmul(X2_inv, np.transpose(tx))
+    # y_p = W.dot(y)
+    loss = compute_loss(y, tx, W)
+    return W, loss
+
+def ridge_regression(y, tx, lambda_):
+    X2 = np.matmul(np.transpose(tx), tx) + lambda_ * np.identity(tx.shape[1])
+    X2_inv = np.linalg.inv(X2)
+    W = np.matmul(X2_inv, np.transpose(tx))
+    # y_p = W.dot(y)
+    loss = compute_loss(y, tx, W)
+    return W, loss
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    w = initial_w
+    for n_iter in range(max_iters):
+        dw = compute_gradient(y, tx, w)
+        w = w- gamma * dw
+
+        # print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+        #       bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    loss = compute_loss(y, tx, w)
+    return w, loss
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    w = initial_w
+    for i in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size = batch_size, shuffle=True):
+            dw = compute_gradient(minibatch_y, minibatch_tx, w)
+            w = w -gamma * dw
+
+        # print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+        #       bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    loss = compute_loss(y, tx, w)    
+    return w, loss
