7f2068add8c578baf958e4be673fd4ac0e6c752f
timothee-florian <timothee-florian.bronner@epfl.ch>
2018-10-12 08:49:45 +0200

    test

--- functions.py
+++ functions.py
@@ -1,3 +1,13 @@
+import numpy as np
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
 def compute_loss(y, tx, w):
     """Calculate the loss (MSE)."""
     N= tx.shape[0]
